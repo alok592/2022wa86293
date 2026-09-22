546fdd5e76139bc29614be8f036e9f55eb697301
alok592 <alok.ac14@gmail.com>
2026-09-22 23:28:00 +0530

    Update App.java

---
+++ DevOpsLab/src/main/java/com/devops/App.java
@@ -0,0 +1,8 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Lab!");
+        System.out.println("Continuous Integration using Jenkins and Maven");
+    }
+}
