33953f9e8b7bafdaa0abc2dc57156d756a01e147
alok592 <alok.ac14@gmail.com>
2026-09-22 23:48:58 +0530

    Update App.java

--- DevOpsLab/src/main/java/com/devops/App.java
+++ DevOpsLab/src/main/java/com/devops/App.java
@@ -2,7 +2,7 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+       System.out.println("Hello from DevOps Lab - CI Test!");
         System.out.println("Continuous Integration using Jenkins and Maven");
     }
 }
